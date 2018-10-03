81d7f5f1261515fd2eac9789d6c9bfae40cacfc0
Jeroen Zegers <jzegers@hommage.esat.kuleuven.be>
2018-10-03 11:31:00 +0200

    updated init

--- nabu/neuralnetworks/loss_computers/__init__.py
+++ nabu/neuralnetworks/loss_computers/__init__.py
@@ -6,4 +6,5 @@ from . import loss_computer_factory, loss_computer, deepclustering_loss, pit_los
   crossentropy_multi_loss, dist2mean_closest_rat_loss, direct_loss,\
   dist2mean_epsilon_closest_rat_loss, dc_pit_loss, crossentropy_multi_loss_reshapelogits, \
   crossentropy_multi_loss_reshapelogits_avtime,\
-  deepclustering_full_crossentropy_multi_reshapedlogits_avtime_loss
+  deepclustering_full_crossentropy_multi_reshapedlogits_avtime_loss, \
+  deepclustering_flat_loss
