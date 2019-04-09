f752f674795de57a00badaa801391ebfdaba49bb
Jeroen Zegers <jzegers@hommage.esat.kuleuven.be>
2019-04-09 18:51:13 +0200

    removed print

--- nabu/neuralnetworks/loss_computers/deepclustering_loss.py
+++ nabu/neuralnetworks/loss_computers/deepclustering_loss.py
@@ -1,15 +1,15 @@
-'''@file deepclustering_loss.py
-contains the DeepclusteringLoss'''
+"""@file deepclustering_loss.py
+contains the DeepclusteringLoss"""
 
-import tensorflow as tf
 import loss_computer
 from nabu.neuralnetworks.components import ops
 
+
 class DeepclusteringLoss(loss_computer.LossComputer):
-    '''A loss computer that calculates the loss'''
+    """A loss computer that calculates the loss"""
 
     def __call__(self, targets, logits, seq_length):
-        '''
+        """
         Compute the loss
 
         Creates the operation to compute the deep clustering loss
@@ -24,15 +24,13 @@ class DeepclusteringLoss(loss_computer.LossComputer):
         Returns:
             loss: a scalar value containing the loss
             norm: a scalar value indicating how to normalize the loss
-        '''
+        """
 
-        binary_target=targets['binary_targets']
+        binary_target = targets['binary_targets']
         usedbins = targets['usedbins']
         seq_length = seq_length['bin_emb']
         logits = logits['bin_emb']
 
-        loss, norm = ops.deepclustering_loss(binary_target, logits, usedbins,
-					seq_length,self.batch_size)
-        print tf.shape(loss)
-        print tf.shape(norm)
+        loss, norm = ops.deepclustering_loss(binary_target, logits, usedbins, seq_length, self.batch_size)
+
         return loss, norm
